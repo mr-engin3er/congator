a4d03f80c5daf315f45b9824d2bfd1522b5d10f0
mr-engin3er <dheerajsanadhya@gmail.com>
2020-09-17 21:41:04 +0530

    add some functionality

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import index
+from .views import (
+    index,
+    new_search,
+)
 app_name = 'home'
 
 urlpatterns = [
     path('', index, name=''),
+    path('new_search', new_search, name='new_search')
+
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
 from django.shortcuts import render
+from .models import Search
+
 
 # Create your views here.
+AMAZONE_URL = 'https://www.amazon.in/s?k={}'
+FLIPKART_URL = 'https://www.flipkart.com/search?q={}'
 
 
 def index(request):
     return render(request, 'home/index.html')
+
+
+def new_search(request):
+    search = request.POST.get('search')
+    # Search.objects.create(search=search)
+    FINAL_AMAZON_URL = AMAZONE_URL.format(requests.compat.quote_plus(search))
+    res = requests.get(FINAL_AMAZON_URL)
+    soup = BeautifulSoup(res.text, features="html.parser")
+    search_title = soup.find_all(
+        'span', {'class': 'a-size-medium a-color-base a-text-normal'})
+    search_price = soup.find_all(
+        'span', {'class': 'a-price-whole'})
+    search_link = soup.find_all(
+        'a', {'class': 'a-link-normal a-text-normal'})
+    print(search_title[0].text)
+    print(search_price[0].text)
+    print(f"https://www.amazon.in{search_link[0].get('href')}")
+    context = {
+        'search': search,
+    }
+    return render(request, 'home/new_search.html', context)
