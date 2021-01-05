36697f3085d34d4617c6496d3c86c72eb3f2ab5d
mr-engin3er <dheerajsanadhya@gmail.com>
2021-01-05 22:09:39 +0530

    improve backend

--- home/views.py
+++ home/views.py
@@ -16,19 +16,49 @@ def index(request):
 def new_search(request):
     search = request.POST.get('search')
     # Search.objects.create(search=search)
-    FINAL_AMAZON_URL = AMAZONE_URL.format(requests.compat.quote_plus(search))
-    res = requests.get(FINAL_AMAZON_URL)
-    soup = BeautifulSoup(res.text, features="html.parser")
-    search_title = soup.find_all(
-        'span', {'class': 'a-size-medium a-color-base a-text-normal'})
-    search_price = soup.find_all(
-        'span', {'class': 'a-price-whole'})
-    search_link = soup.find_all(
-        'a', {'class': 'a-link-normal a-text-normal'})
-    print(search_title[0].text)
-    print(search_price[0].text)
-    print(f"https://www.amazon.in{search_link[0].get('href')}")
+
+    def amazon(search):
+        FINAL_AMAZON_URL = AMAZONE_URL.format(
+            requests.compat.quote_plus(search))
+        response = requests.get(FINAL_AMAZON_URL)
+        soup = BeautifulSoup(response.text, features="html.parser")
+        search_title = soup.find_all(
+            'span', {'class': 'a-size-medium a-color-base a-text-normal'})
+        search_price = soup.find_all(
+            'span', {'class': 'a-price-whole'})
+        search_link = soup.find_all(
+            'a', {'class': 'a-link-normal a-text-normal'})
+        search_photo = soup.find_all('img', {'class': 's-image'})
+        amazon_title = search_title[0].text
+        amazon_price = search_price[0].text
+        amazon_url = f"https://www.amazon.in{search_link[0].get('href')}"
+        amazon_photo = search_photo[0].get('src')
+        return {'amazon_title': amazon_title,
+                'amazon_price': amazon_price,
+                'amazon_url': amazon_url,
+                'amazon_photo': amazon_photo}
+
+    def flipkart(search):
+        FINAL_FLIPKART_URL = FLIPKART_URL.format(
+            requests.compat.quote_plus(search))
+        response = requests.get(FINAL_FLIPKART_URL)
+        soup = BeautifulSoup(response.text, features="html.parser")
+        search_title = soup.find_all(
+            'div', {'class': '_4rR01T'})
+        search_price = soup.find_all(
+            'div', {'class': '_30jeq3 _1_WHN1'})
+        search_link = soup.find_all(
+            'a', {'class': '_1fQZEK'})
+        flipkart_title = search_title[0].text
+        flipkart_price = search_price[0].text
+        flipkart_url = f"https://www.flipkart.com{search_link[0].get('href')}"
+        return {'flipkart_title': flipkart_title,
+                'flipkart_price': flipkart_price,
+                'flipkart_url': flipkart_url}
     context = {
         'search': search,
     }
+    context.update(amazon(search))
+    context.update(flipkart(search))
+    print(context)
     return render(request, 'home/new_search.html', context)
